6f23c80d9f60e0769fd7fd7bf64c1f8c5ce23e67
Mohsen <mohsen.mahabadi@rabobank.nl>
2023-05-25 10:03:03 +0200

    add bookmark button to the youtube player

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,56 @@
 (() => {
+  const bookmarkBtnId = "yt-chrome-extension-bookmark-btn";
+
   chrome.runtime.onMessage.addListener((obj, sender, response) => {
-    console.log("response:", obj);
+    console.log("response", obj);
   });
+
+  const generateIcon = () => {
+    const container = document.createElement("div");
+
+    const iconSvg = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+    const iconPath = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "path"
+    );
+
+    container.setAttribute("id", bookmarkBtnId);
+    container.style.cursor = "pointer";
+
+    iconSvg.setAttribute("fill", "white");
+    iconSvg.setAttribute("viewBox", "0 0 91.5 122.88");
+    iconSvg.style.opacity = "0.7";
+    iconSvg.style.width = "30px";
+    iconSvg.style.height = "30px";
+    iconSvg.style.marginTop = "10px";
+
+    iconPath.setAttribute(
+      "d",
+      "M62.42,0A29.08,29.08,0,1,1,33.34,29.08,29.08,29.08,0,0,1,62.42,0ZM3.18,19.65H24.73a38,38,0,0,0-1,6.36H6.35v86.75L37.11,86.12a3.19,3.19,0,0,1,4.18,0l31,26.69V66.68a39.26,39.26,0,0,0,6.35-2.27V119.7a3.17,3.17,0,0,1-5.42,2.24l-34-29.26-34,29.42a3.17,3.17,0,0,1-4.47-.33A3.11,3.11,0,0,1,0,119.7H0V22.83a3.18,3.18,0,0,1,3.18-3.18Zm55-2.79a4.1,4.1,0,0,1,.32-1.64l0-.06a4.33,4.33,0,0,1,3.9-2.59h0a4.23,4.23,0,0,1,1.63.32,4.3,4.3,0,0,1,1.39.93,4.15,4.15,0,0,1,.93,1.38l0,.07a4.23,4.23,0,0,1,.3,1.55v8.6h8.57a4.3,4.3,0,0,1,3,1.26,4.23,4.23,0,0,1,.92,1.38l0,.07a4.4,4.4,0,0,1,.31,1.49v.18a4.37,4.37,0,0,1-.32,1.55,4.45,4.45,0,0,1-.93,1.4,4.39,4.39,0,0,1-1.38.92l-.08,0a4.14,4.14,0,0,1-1.54.3H66.71v8.57a4.35,4.35,0,0,1-1.25,3l-.09.08a4.52,4.52,0,0,1-1.29.85l-.08,0a4.36,4.36,0,0,1-1.54.31h0a4.48,4.48,0,0,1-1.64-.32,4.3,4.3,0,0,1-1.39-.93,4.12,4.12,0,0,1-.92-1.38,4.3,4.3,0,0,1-.34-1.62V34H49.56a4.28,4.28,0,0,1-1.64-.32l-.07,0a4.32,4.32,0,0,1-2.25-2.28l0-.08a4.58,4.58,0,0,1-.3-1.54v0a4.39,4.39,0,0,1,.33-1.63,4.3,4.3,0,0,1,3.93-2.66h8.61V16.86Z"
+    );
+    iconPath.setAttribute("style", "fill-rule:evenodd");
+
+    iconSvg.appendChild(iconPath);
+    container.appendChild(iconSvg);
+
+    return container;
+  };
+
+  const newVideoLoaded = () => {
+    const bookmarkBtnExist = document.getElementById(bookmarkBtnId);
+
+    if (!bookmarkBtnExist) {
+      const bookmarkBtn = generateIcon();
+
+      const youtubeControlls =
+        document.getElementsByClassName("ytp-left-controls")[0];
+
+      youtubeControlls.appendChild(bookmarkBtn);
+    }
+  };
+
+  newVideoLoaded();
 })();
